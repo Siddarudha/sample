cef0a4c2ce9a4715c19f4d33cc812ddb2746b6fc
Siddarudha <42236536+Siddarudha@users.noreply.github.com>
2024-02-08 12:19:28 +0530

    Committed latest file

--- TestProgram.java
+++ TestProgram.java
@@ -6,25 +6,29 @@ package TestScripts;
 public class TestProgram {
 
 	public static void main(String[] args) throws Exception {
-		
-		 int a = 50, b = 20, c; c = a > b ? a : b; 
-		 System.out.println(c);
-		 
 
-		/*
-		 * String rev = ""; String str = "Automation Program is Very Good"; for (int i =
-		 * 0; i < str.length(); i++) { char ch = str.charAt(i); rev = ch + rev; }
-		 * System.out.println(rev);
-		 */
-		int rev = 0;
+		// Print largest number among two
+		int a = 50, b = 20, c;
+		c = a > b ? a : b;
+		System.out.println(c);
+// Print reverse of the given string
+		String rev = "";
+		String str = "Automation Program is Very Good";
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			rev = ch + rev;
+		}
+		System.out.println(rev);
+// Print reverse of the given number
+		int revs = 0;
 		int num = 123456789;
 		while (num != 0) {
 
 			int number = num % 10;
-			rev = rev * 10 + number;
+			revs = revs * 10 + number;
 			num = num / 10;
 		}
-		System.out.println(rev);
+		System.out.println(revs);
 	}
 
 }
